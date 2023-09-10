5e851bee25307c252d9c82f98f058172df32e697
RadhikaReddy <radhika0reddy031@gmail.com>
2023-09-10 18:53:01 -0400

    s5

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit{
+  model:any={}
+// loggedIn=false;
+  constructor(public accountService:AccountService) {}
+  ngOnInit(): void {
+  }
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+      },
+      error: error => console.log(error) 
+    })
+  }
+  logout() {
+    this.accountService.logout();
+  }
+}
