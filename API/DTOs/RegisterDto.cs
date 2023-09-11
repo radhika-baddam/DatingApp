dc683044edd7af310a0c8796daea3d5e518d68d3
RadhikaReddy <radhika0reddy031@gmail.com>
2023-09-11 13:06:18 -0400

    S7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -7,5 +7,6 @@ public class RegisterDto
 [Required]
 public string Username { get; set; }
 [Required]
+[StringLength(8, MinimumLength = 3)]
 public string Password { get; set; }
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
@@ -8,22 +10,24 @@ import { ToastrService } from 'ngx-toastr';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit{
-  model:any={}
-// loggedIn=false;
-  constructor(public accountService:AccountService,private router: Router,private toastr:ToastrService) {}
+export class NavComponent implements OnInit {
+  model: any = {}
+
+  constructor(public accountService: AccountService, private router: Router, 
+    private toastr: ToastrService) { }
+
   ngOnInit(): void {
   }
+
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        this.router.navigateByUrl('/members')
-      },
-      error: error => this.toastr.error(error.error)
+      next: _ => this.router.navigateByUrl('/members')
     })
   }
+
   logout() {
     this.accountService.logout();
     this.router.navigateByUrl('/');
   }
+
 }
